d4c1aff4135b3740eb80c94305f48d5b2376c805
ratraparth59 <ratraparth59@gmail.com>
2024-07-08 00:47:34 +0530

    new package

---
+++ dist/routes/site.routes.js
@@ -0,0 +1,12 @@
+import Router from "express";
+const router = Router();
+import { createSite } from "../controllers/Sites.controller";
+import { verifyJWT } from "../middleware/auth.middleware";
+import { getDronesBySiteName } from "../controllers/Sites.controller";
+import { updateSiteByName } from "../controllers/Sites.controller";
+import { deleteSiteByID } from "../controllers/Sites.controller";
+router.post("/createSite", verifyJWT, createSite);
+router.get("/getdroneBySiteName", verifyJWT, getDronesBySiteName);
+router.put("/updateSiteName", verifyJWT, updateSiteByName);
+router.delete("/deleteSiteByID/:_id", verifyJWT, deleteSiteByID);
+export default router;
---
+++ dist/routes/user.routes.js
@@ -0,0 +1,11 @@
+import route from "express";
+const router = route();
+import { UserSignup, logoutUser } from "../controllers/User.controller";
+import { loginUser } from "../controllers/User.controller";
+import { getUserById } from "../controllers/User.controller";
+import { verifyJWT } from "../middleware/auth.middleware";
+router.post("/signup", UserSignup);
+router.post("/login", loginUser);
+router.get("/getUserById/:uID", getUserById);
+router.post("/logout", verifyJWT, logoutUser);
+export default router;
