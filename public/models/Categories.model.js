95b975fe34423ede23e5fd7a49e217335580d354
ratraparth59 <ratraparth59@gmail.com>
2024-07-08 13:58:25 +0530

    backend problem fix

--- public/models/Categories.model.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-const CategorySchema = new Schema({
-    name: { type: String, required: true },
-    color: { type: String, required: true },
-    tag_name: { type: String, required: true },
-}, {
-    timestamps: true,
-});
-export const Category = model("Categories", CategorySchema);
